ce19be9a68e1ae669e022b338785dd52da9e664f
Daniel Aguilera Millán <daguilera@sklum.com>
2022-03-22 13:06:37 +0100

    test

--- src/index.ts
+++ src/index.ts
@@ -17,14 +17,13 @@ async function getTopics(): Promise<any> {
 }
 
 function genApiDocs(): void {
-    const result = exec('DOC_API_ACTIVE=true GENERATE_DOCUMENTATION_JSON=true npx nest start', (error, stdout, stderr) => {
-        console.log(error, stdout, stderr);
-    });
-    console.log(result);
+    const result = exec('DOC_API_ACTIVE=true GENERATE_DOCUMENTATION_JSON=true npx nest start');
+    console.log(result.stdout);
     pushCommit();
 }
 
 function pushCommit(): void {
+    console.log("HEHEHE")
     exec(`git config --global user.email "actions@github.com" && \
             git config --global user.name "Github Action" && \
             git add openapi.json && \
