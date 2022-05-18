7f8985daf8ffeb34f689f8f874c2cb7096dda6b0
Daniel Aguilera <daguilera@sklum.com>
2022-05-18 13:28:32 +0200

    fix(MS-298): change to list of topics, debug => info & file creation check

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { debug, error, getInput, setFailed, setOutput } from '@actions/core';
+import { info, error, getInput, setFailed, setOutput } from '@actions/core';
 import { Octokit } from "@octokit/rest";
 import { exec } from 'child_process';
 const fs = require('fs')
@@ -22,16 +22,16 @@ async function genApiDocs() {
     const openApiPath = './openapi.json';
     let apiVersion = '1.0.0';
 
-    debug('Checking if openapi file exists...');
+    info('Checking if openapi file exists...');
     if (fs.existsSync(openApiPath)) {
-        debug('Openapi file found, checking if version has changed...');
+        info('Openapi file found, checking if version has changed...');
         try{
             const openApiFile = fs.readFileSync(openApiPath);
 
             apiVersion = JSON.parse(openApiFile)?.info?.version;
-            debug(`Current api version is ${apiVersion}`);
+            info(`Current api version is ${apiVersion}`);
             exec('git tag -l apiV*', (err, stdout, stderr) => {
-                debug(`Current tags are ${stdout}`);
+                info(`Current tags are ${stdout}`);
                 if(err) {
                     error(err.message);
                     throw new Error(err.message);
@@ -41,8 +41,8 @@ async function genApiDocs() {
                     throw new Error(stderr);
                 }
                 else if(!stdout.includes(apiVersion)){
-                    debug('Version not found in previous tags, updating api documentation')
-                    updateOpenApiFile(apiVersion);
+                    info('Version not found in previous tags, updating api documentation')
+                    updateOpenApiFile(apiVersion, openApiPath);
                 }
             });
         }
@@ -51,22 +51,31 @@ async function genApiDocs() {
         }
     }
     else {
-        updateOpenApiFile(apiVersion);
+        info('Openapi file not found');
+        updateOpenApiFile(apiVersion, openApiPath);
     }
 }
 
-async function updateOpenApiFile(version) {
+async function updateOpenApiFile(version, filePath) {
+    info('Generating openapi file...');
     await exec(`DOC_API_ACTIVE=true GENERATE_DOCUMENTATION_JSON=true DOC_API_VERSION=${version} DOC_API_TITLE=${repo} npx nest start`);
-    setOutput('commitDoc', true);
-    setOutput('apiVersion', version);
+    if (fs.existsSync(filePath)) {
+        info('Done!');
+        setOutput('commitDoc', true);
+        setOutput('apiVersion', version);
+    }
+    else {
+        throw new Error('Error generating file');
+    }
+
 }
 
 async function main(): Promise<void> {
     const { data } = await getTopics();
-    const topic = data.names[0];
+    const topics = data.names;
 
-    debug(`Topic: ${topic}, Core nest module minor version: ${packageVersion}`);
-    if(topic === 'microservice' && packageVersion >= 27) {
+    info(`Is microservice: ${topics.includes('microservice')}, Core nest module minor version: ${packageVersion}`);
+    if(topics.includes('microservice') && packageVersion >= 27) {
         await genApiDocs();
     }
 }
